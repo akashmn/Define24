8dcdc0a449ff21277907da7747870cdde0afa56f
edwineas <edwinalexshaji@gmail.com>
2024-04-21 09:15:41 +0530

    login css

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,11 @@ function App() {
   return (
    <>
 
-    <Dashboard/>
+    {/* <Dashboard/> */}
     <Login/>
-    <Register/>
-    <History/>
-    <ActiveCases/>
+    {/* <Register/> */}
+    {/* <History/> */}
+    {/* <ActiveCases/> */}
    </>
   )
 }
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import '../Styles/Login.css'
 
 const Register = () => {
 
