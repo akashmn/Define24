1c42187112b459ee53c649f33169ecf93cc5e0c0
edwineas <edwinalexshaji@gmail.com>
2024-04-21 10:48:23 +0530

    login function

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react';
+// import { useHistory } from 'react-router-dom';
 
 const Login = () => {
-  const [username, setUsername] = useState('');
+  const [email, setemail] = useState('');
   const [password, setPassword] = useState('');
+  // const history = useHistory();
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    // Handle login logic here
+
+    const response = await fetch('https://refracto-backend.onrender.com/departments/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ adminEmail: email, adminPassword: password })
+    });
+
+    const data = await response.json();
+    const dataset = data.response
+
+    if (dataset.status === 1) {
+      localStorage.setItem("departmentID", dataset.data.departmentID);
+      localStorage.setItem("adminName", dataset.data.adminName);
+      localStorage.setItem("departmentName", dataset.data.departmentName);
+      // history.push('/dashboard');
+    }
+
   };
 
   return (
@@ -15,23 +35,23 @@ const Login = () => {
         <h1 className="text-2xl font-semibold mb-6 text-center">Sign in to your account</h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">Email address</label>
-            <input 
-              type="email" 
-              id="email" 
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email address</label>
+            <input
+              type="email"
+              id="email"
               className="w-full p-2 border rounded-md"
-              value={username} 
-              onChange={(e) => setUsername(e.target.value)} 
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
             />
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input 
-              type="password" 
-              id="password" 
+            <input
+              type="password"
+              id="password"
               className="w-full p-2 border rounded-md"
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button type="submit" className="w-full py-2 px-4 text-white rounded-md submitbutton">Submit</button>
